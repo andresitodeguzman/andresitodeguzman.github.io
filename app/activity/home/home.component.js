6ecee92f33d87b175402aa70de5dbaa6d245025d
Andresito de Guzman <andresitomyemail@gmail.com>
2019-04-19 00:56:36 +0800

    Try typewriter effect

--- app/activity/home/home.component.js
+++ app/activity/home/home.component.js
@@ -4,6 +4,20 @@ angular.module('home',['menubar','fixedbar','footerbar']).component('home',{
   controller: function homeController(){
       this.$onInit = ()=>{
         window.data.currentPage = "About Me";
+
+	var tp = document.getElementById('faceTypewriter');
+	var typewriter = new Typewriter(tp,{loop:true});
+
+	typewriter.typeString('Human Biology')
+	    .pauseFor(2500)
+	    .deleteAll()
+	    .typeString('Progressive Web App')
+	    .pauseFor(2500)
+	    .deleteAll()
+	    .typeString('User Experience')
+	    .pauseFor(2500)
+	    .start();
+
       };
   }
     
